cdd940ddd1f4553ffa265ee654fb07afb06c4798
ElenaVak <Vak1977.1977@gmail.com>
2020-04-23 12:06:33 +0300

    Change html file to semantic.Change the scroll button.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
+function backToTop() {
+  let button = $('.back-to-top');
 
+  $(window).on('scroll', () => {
+    if ($(this).scrollTop() >= 50) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
 
+  button.on('clik', (e) => {
+    e.preventDefault();
+    $('html').animate({scrollTop: 0}, 1000);
+  })
+}
+          
+ backToTop();         
+        
+         
+
+          
+   
+  
