b57663923bc9c72d5a09e11f0a15b221f9a6b91f
ElenaVak <Vak1977.1977@gmail.com>
2020-05-02 12:20:40 +0300

    Port 3000

---
+++ node-server-example/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const port = 3000
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/hello', function(req, res) {
+    res.send("Hello There!")
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/about.html'))
+})
+
+
+app.listen(port, function (){
+    console.log(`Example app lisening on port ${port}!`)
+})
