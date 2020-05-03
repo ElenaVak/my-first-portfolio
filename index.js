4b36be65b8296a6d269f0ee6ec3ecc21ff7c95d0
ElenaVak <Vak1977.1977@gmail.com>
2020-05-03 19:58:10 +0300

    localhost to all pages

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ app.get('/hello', function(req, res) {
     res.send("Hello There!")
 });
 
+app.get('/index', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/index.html'))
+});
+
 app.get('/about', function(req, res) {
     res.sendFile(path.join(__dirname+'/public/about.html'))
-})
+});
 
+app.get('/experience', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/experience.html'))
+});
+
+app.get('/skills', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/skills.html'))
+});
 
 app.listen(port, function (){
-    console.log(`Example app lisening on port ${port}!`)
-})
+    console.log(`Example app lisening on port ${port}!`);
+});
