9419a192c0c1bcc9a5c9e17b74dab3a86bb69bac
ElenaVak <Vak1977.1977@gmail.com>
2020-05-05 18:32:11 +0300

    added a template using Hendlebars

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const path = require("path");
 const express = require("express");
+var exphbs = require('express-handlebars');
 const port = 3000
 
 const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+    });
 
 app.get('/hello', function(req, res) {
     res.send("Hello There!")
 });
 
-app.get('/index', function(req, res) {
-    res.sendFile(path.join(__dirname+'/public/index.html'))
-});
+
 
 app.get('/about', function(req, res) {
     res.sendFile(path.join(__dirname+'/public/about.html'))
